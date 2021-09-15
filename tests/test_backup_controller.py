b02bfbfdc197ea7b90c8fd4b7983dc48a48ecd89
AlexProtchenko <ialexprotchenko@gmail.com>
2021-09-15 12:44:33 +0300

    add test

--- tests/test_backup_controller.py
+++ tests/test_backup_controller.py
@@ -19,3 +19,23 @@ def test_backup_create_data():
     code = str(base64.b64encode(binary_input))
     code2 = response.text
     assert code[2:-1] == code2[3:-3]
+
+
+def test_backup_error():
+    with open('./tests/utils/big.jpg', 'rb') as file:
+        binary_input = file.read()
+    response = requests.request(method='POST',
+                                url='http://127.0.0.1:5000/api/backups/save',
+                                data=binary_input
+                                )
+    assert response.status_code == 400
+    assert response.json() == {'message': 'Not allowed size', 'statusCode': 400}
+
+    with open('./tests/utils/small.jpeg', 'rb') as file:
+        binary_input = file.read()
+    response = requests.request(method='POST',
+                                url='http://127.0.0.1:5000/api/backups/save',
+                                data=binary_input
+                                )
+    assert response.status_code == 400
+    assert response.json() == {'message': 'Not allowed size', 'statusCode': 400}
